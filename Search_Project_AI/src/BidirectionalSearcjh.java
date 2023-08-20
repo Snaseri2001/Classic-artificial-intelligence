c14bf966e453eff968279085a4e89f504dc2c938
Soroush Naseri <60386309+Snaseri2001@users.noreply.github.com>
2023-08-20 11:56:04 +0330

    Update BidirectionalSearcjh.java

--- Search_Project_AI/src/BidirectionalSearcjh.java
+++ Search_Project_AI/src/BidirectionalSearcjh.java
@@ -10,158 +10,143 @@ import java.util.Queue;
 import java.util.Stack;
 import java.util.Enumeration ;
 
-    /**
-     *
-     * @author Patric
-     */
-    public class BidirectionalSearcjh {
+public class BidirectionalSearch {
 
-    public static Stack<State>  Result = new Stack<State>();
+    // Stack to store the result path
+    public static Stack<State> Result = new Stack<State>();
 
-        public static void BidirectioanlSearch(State initialState , State Goal){
-            Queue<State> frontier1 = new LinkedList<State>();
-            Hashtable<String, Boolean>  inFrontier1 = new Hashtable<>();
-            Hashtable<String, Boolean>  explored1 = new Hashtable<>();
+    // Method to perform bidirectional search
+    public static void BidirectionalSearch(State initialState, State goal) {
+        // Frontier and explored sets for the forward search
+        Queue<State> frontier1 = new LinkedList<State>();
+        Hashtable<String, Boolean> inFrontier1 = new Hashtable<>();
+        Hashtable<String, Boolean> explored1 = new Hashtable<>();
 
-            frontier1.add(initialState);
-            inFrontier1.put(initialState.hash(),true);
+        // Frontier and explored sets for the backward search
+        Queue<State> frontier2 = new LinkedList<State>();
+        Hashtable<String, Boolean> inFrontier2 = new Hashtable<>();
+        Hashtable<String, Boolean> explored2 = new Hashtable<>();
 
-            Queue<State> frontier2 = new LinkedList<State>();
-            Hashtable<String, Boolean>  inFrontier2 = new Hashtable<>();
-            Hashtable<String, Boolean>  explored2 = new Hashtable<>();
+        // Add the initial state to the forward frontier
+        frontier1.add(initialState);
+        inFrontier1.put(initialState.hash(), true);
 
-            frontier2.add(Goal);
-            inFrontier2.put(Goal.hash(),true);
+        // Add the goal state to the backward frontier
+        frontier2.add(goal);
+        inFrontier2.put(goal.hash(), true);
 
+        // Variable to store the intersection state
+        int intersection = -1;
 
-            int interSection = -1;
+        // Perform the bidirectional search
+        while (!frontier1.isEmpty() && !frontier2.isEmpty() && intersection == -1) {
+            // Perform BFS search on the forward frontier
+            BFSsearch(frontier1, inFrontier1, explored1);
 
-            while( !frontier1.isEmpty() && !frontier2.isEmpty() && interSection  == -1 ){
+            // Perform BFS search on the backward frontier
+            BFSsearch(frontier2, inFrontier2, explored2);
 
-                BFSsearch(  frontier1 ,  inFrontier1 , explored1 ) ;
-                BFSsearch(  frontier2 ,  inFrontier2 , explored2 ) ;
-
-               boolean in2 = false ;
-               String keyGoal ="";
-                Enumeration<String> enumeration = inFrontier1.keys();
-                while (enumeration.hasMoreElements()){
-                    String key =enumeration.nextElement() ;
-
-                    if(inFrontier2.containsKey(key)){
-                        keyGoal = key ;
-                        in2 = true ;
-                        break ;}}
-
-                       if(in2 == true){
-                           int c1 = frontier2.size() ;
-                        for(int i = 0; i < c1 ; i++){
-                            State state1= frontier2.poll() ;
-                            if(state1.hash().equals(keyGoal) ){
-                                Stack<State>  firstResult = new Stack<State>();
-                                while (true){
-                                    firstResult.add(state1);
-                                    if(state1.getParentState() == null){
-                                        break;
-                                    }
-                                    else {
-                                        state1 = state1.getParentState();
-                                    }
-                                }
-                                int c = firstResult.size() ;
-                                for(int ui = 0 ; ui < c ; ui ++ ){
-                                    Result.add(firstResult.pop()) ;
-                                }
-                                break ;
-                            }
-
-
-
-                        }
-
-                    int c2 = frontier1.size() ;
-                    for(int i = 0; i < c2 ; i++){
-                        State state1= frontier1.poll() ;
-                        if(state1.hash().equals(keyGoal)){
-                        state1 = state1.getParentState() ;
-                            while (true){
-                                Result.add(state1);
-                                if(state1.getParentState() == null){
-                                    break;
-                                }
-                                else {
-                                    state1 = state1.getParentState();
-                                }
-                            }
-
-
-                            break ;
-                        }
-
-                    }
-                        result() ;
-                    break ;
-                    }
-
+            // Check if there is an intersection between the frontiers
+            boolean intersectionFound = false;
+            String intersectionKey = "";
+            Enumeration<String> enumeration = inFrontier1.keys();
+            while (enumeration.hasMoreElements()) {
+                String key = enumeration.nextElement();
+                if (inFrontier2.containsKey(key)) {
+                    intersectionKey = key;
+                    intersectionFound = true;
+                    break;
                 }
-
             }
 
-
-
-
-
-
-
-        public static void BFSsearch( Queue<State> frontier , Hashtable<String, Boolean>  inFrontier , Hashtable<String, Boolean> explored ){
-
-
-            State tempState = frontier.poll();
-            inFrontier.remove(tempState.hash());
-            explored.put(tempState.hash(),true);
-            ArrayList<State> children = tempState.successor();
-            for(int i = 0;i<children.size();i++){
-                if(!(inFrontier.containsKey(children.get(i).hash()))
-                        && !(explored.containsKey(children.get(i).hash()))) {
-
-                    frontier.add(children.get(i));
-                    inFrontier.put(children.get(i).hash(), true);
+            // If there is an intersection, construct the result path
+            if (intersectionFound) {
+                // Construct the path from the goal to the intersection
+                int frontier2Size = frontier2.size();
+                for (int i = 0; i < frontier2Size; i++) {
+                    State state1 = frontier2.poll();
+                    if (state1.hash().equals(intersectionKey)) {
+                        Stack<State> firstResult = new Stack<State>();
+                        while (true) {
+                            firstResult.add(state1);
+                            if (state1.getParentState() == null) {
+                                break;
+                            } else {
+                                state1 = state1.getParentState();
+                            }
+                        }
+                        int firstResultSize = firstResult.size();
+                        for (int j = 0; j < firstResultSize; j++) {
+                            Result.add(firstResult.pop());
+                        }
+                        break;
+                    }
                 }
 
+                // Construct the path from the intersection to the initial state
+                int frontier1Size = frontier1.size();
+                for (int i = 0; i < frontier1Size; i++) {
+                    State state1 = frontier1.poll();
+                    if (state1.hash().equals(intersectionKey)) {
+                        state1 = state1.getParentState();
+                        while (true) {
+                            Result.add(state1);
+                            if (state1.getParentState() == null) {
+                                break;
+                            } else {
+                                state1 = state1.getParentState();
+                            }
+                        }
+                        break;
+                    }
+                }
+
+                // Output the result
+                result();
+                break;
             }
         }
-
-
-
-        private static void result(){
-
-
-            try {
-                FileWriter myWriter = new FileWriter("BidirectionalResult.txt");
-                System.out.println("initial state : ");
-                while (!Result.empty()){
-                    State tempState = Result.pop();
-                    if(tempState.getSelectedNodeId() != -1) {
-                        System.out.println("selected id : " + tempState.getSelectedNodeId());
-                    }
-                    tempState.getGraph().print();
-
-                    myWriter.write(tempState.getSelectedNodeId()+" ,");
-                    myWriter.write(tempState.outputGenerator()+"\n");
-                }
-                myWriter.close();
-                System.out.println("Successfully wrote to the file.");
-            } catch (IOException e) {
-                System.out.println("An error occurred.");
-                e.printStackTrace();
-            }
-        }
-
-
-
     }
 
+    // Method to perform BFS search
+    public static void BFSsearch(Queue<State> frontier, Hashtable<String, Boolean> inFrontier,
+            Hashtable<String, Boolean> explored) {
+        State tempState = frontier.poll();
+        inFrontier.remove(tempState.hash());
+        explored.put(tempState.hash(), true);
+        ArrayList<State> children = tempState.successor();
+        for (int i = 0; i < children.size(); i++) {
+            if (!(inFrontier.containsKey(children.get(i).hash()))
+                    && !(explored.containsKey(children.get(i).hash()))) {
+                frontier.add(children.get(i));
+                inFrontier.put(children.get(i).hash(), true);
+            }
+        }
+    }
 
+    // Method to output the result path
+    private static void result() {
+        try {
+            FileWriter myWriter = new FileWriter("BidirectionalResult.txt");
+            System.out.println("initial state : ");
+            while (!Result.empty()) {
+                State tempState = Result.pop();
+                if (tempState.getSelectedNodeId() != -1) {
+                    System.out.println("selected id : " + tempState.getSelectedNodeId());
+                }
+                tempState.getGraph().print();
 
+                myWriter.write(tempState.getSelectedNodeId() + " ,");
+                myWriter.write(tempState.outputGenerator() + "\n");
+            }
+            myWriter.close();
+            System.out.println("Successfully wrote to the file.");
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+}
 
 
 
