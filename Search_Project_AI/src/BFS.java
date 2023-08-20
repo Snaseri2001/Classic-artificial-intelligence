3365f716a35ac149a098da9eb390e545525d2408
Soroush Naseri <60386309+Snaseri2001@users.noreply.github.com>
2023-08-20 11:54:00 +0330

    Update BFS.java

--- Search_Project_AI/src/BFS.java
+++ Search_Project_AI/src/BFS.java
@@ -5,26 +5,34 @@ import java.util.Hashtable;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Stack;
+public class BFS {
 
- public class  BFS {
-
-    public static void search(State initialState){
+    // This method performs the Breadth-First Search algorithm
+    public static void search(State initialState) {
         Queue<State> frontier = new LinkedList<State>();
         Hashtable<String, Boolean> inFrontier = new Hashtable<>();
         Hashtable<String, Boolean> explored = new Hashtable<>();
-        if(isGoal(initialState)){
+
+        // Check if the initial state is the goal state
+        if (isGoal(initialState)) {
             result(initialState);
             return;
         }
+
+        // Add the initial state to the frontier
         frontier.add(initialState);
-        inFrontier.put(initialState.hash(),true);
-        while (!frontier.isEmpty()){
+        inFrontier.put(initialState.hash(), true);
+
+        // Explore the graph using BFS
+        while (!frontier.isEmpty()) {
             State tempState = frontier.poll();
             inFrontier.remove(tempState.hash());
-            explored.put(tempState.hash(),true);
+            explored.put(tempState.hash(), true);
             ArrayList<State> children = tempState.successor();
-            for(int i = 0;i<children.size();i++){
-                if(!(inFrontier.containsKey(children.get(i).hash()))
+
+            // Check if the goal state is found
+            for (int i = 0; i < children.size(); i++) {
+                if (!(inFrontier.containsKey(children.get(i).hash()))
                         && !(explored.containsKey(children.get(i).hash()))) {
                     if (isGoal(children.get(i))) {
                         result(children.get(i));
@@ -37,39 +45,42 @@ import java.util.Stack;
         }
     }
 
-    private static boolean isGoal(State state){
+    private static boolean isGoal(State state) {
+        // Check if the state is the goal state
         for (int i = 0; i < state.getGraph().size(); i++) {
-            if(state.getGraph().getNode(i).getColor() == Color.Red
-                    || state.getGraph().getNode(i).getColor() == Color.Black){
+            if (state.getGraph().getNode(i).getColor() == Color.Red
+                    || state.getGraph().getNode(i).getColor() == Color.Black) {
                 return false;
             }
         }
         return true;
     }
 
-    private static void result(State state){
-        Stack<State>  states = new Stack<State>();
-        while (true){
+    private static void result(State state) {
+        // Store the path to the goal state in a stack
+        Stack<State> states = new Stack<State>();
+        while (true) {
             states.push(state);
-            if(state.getParentState() == null){
+            if (state.getParentState() == null) {
                 break;
-            }
-            else {
+            } else {
                 state = state.getParentState();
             }
         }
+
+        // Write the result to a file
         try {
             FileWriter myWriter = new FileWriter("BfsResult.txt");
             System.out.println("initial state : ");
-            while (!states.empty()){
+            while (!states.empty()) {
                 State tempState = states.pop();
-                if(tempState.getSelectedNodeId() != -1) {
+                if (tempState.getSelectedNodeId() != -1) {
                     System.out.println("selected id : " + tempState.getSelectedNodeId());
                 }
                 tempState.getGraph().print();
 
-                myWriter.write(tempState.getSelectedNodeId()+" ,");
-                myWriter.write(tempState.outputGenerator()+"\n");
+                myWriter.write(tempState.getSelectedNodeId() + " ,");
+                myWriter.write(tempState.outputGenerator() + "\n");
             }
             myWriter.close();
             System.out.println("Successfully wrote to the file.");
@@ -78,6 +89,4 @@ import java.util.Stack;
             e.printStackTrace();
         }
     }
-}
-
-
+}  
